3a5504804479e0a78eab96a867cc27f07d66c130
bigrushdog <randall.rushing@gmail.com>
2018-11-21 20:03:32 +0100

    bringup: Fontservice
    Pie Bringup by @ZeNiXxX
    
    Change-Id: Ifa4dc6d2d066dc64f14d198d477ca1159d2ec212

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1308,6 +1308,7 @@ public final class Settings {
     public static final String
             ACTION_SHOW_REGULATORY_INFO = "android.settings.SHOW_REGULATORY_INFO";
 
+
     /**
      * Activity Action: Show Device Name Settings.
      * <p>
@@ -4102,6 +4103,20 @@ public final class Settings {
           * @hide
           */
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
+         * Hold current font name and name of the originating package Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_INFO = "font_info";
+
+         /**
+         * Hold list of installed font provider packages. Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_PACKAGES = "font_packages";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -13374,3 +13389,4 @@ public final class Settings {
         return packages[0];
     }
 }
+
